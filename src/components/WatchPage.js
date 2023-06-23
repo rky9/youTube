045b03fdc8d991ee5803a7ac0c53d9c66b112f31
rajesh <rajesh.coer@gmail.com>
2023-06-23 21:25:19 +0530

    Comments with nested comments(comment with reply)

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { closeMenu } from "../utils/appSlice";
+import CommentContainer from "./CommentContainer";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -13,13 +14,17 @@ const WatchPage = () => {
     dispatch(closeMenu());
   }, []);
   return (
-    <div>
+    <div className="flex flex-col">
       <iframe
         width="900"
         height="500"
         title="Youtube video player"
         src={"https://www.youtube.com/embed/" + searchParams.get("v")}
       ></iframe>
+
+      <div>
+        <CommentContainer />
+      </div>
     </div>
   );
 };
